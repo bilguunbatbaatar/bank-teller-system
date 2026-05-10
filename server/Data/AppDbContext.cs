75b1a6eb8f0cfcfca4a7bb1c2af08276a619edc1
Bilguun <Bilguun@DESKTOP-UL13F3Q>
2026-05-10 09:51:04 -0700

    feat: add app db context

---
+++ server/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Ticket> Tickets => Set<Ticket>();
+
+    public DbSet<Account> Accounts => Set<Account>();
+
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    public DbSet<ExchangeRate> ExchangeRates => Set<ExchangeRate>();
+
+    public DbSet<Teller> Tellers => Set<Teller>();
+}
