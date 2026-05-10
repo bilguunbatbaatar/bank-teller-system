33ddbfb53cbfb80fd922dae86ce34aedbf51cb9c
Bilguun <Bilguun@DESKTOP-UL13F3Q>
2026-05-10 10:00:46 -0700

    feat: configure database connection

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using server.Data;
 var builder = WebApplication.CreateBuilder(args);
-
+// Enendeer anhaaral handuulaarai! ( guur bolj ugch baigaa server, db hoyriin hoorond)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 
 builder.Services.AddControllers();
