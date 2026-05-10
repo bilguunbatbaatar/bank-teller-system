0fbdf4a3711f46d6b05bc079aa1cc4eccdae1585
Bilguun <Bilguun@DESKTOP-UL13F3Q>
2026-05-10 09:36:17 -0700

    feat: add transaction model

---
+++ server/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+
+    public string AccountNumber { get; set; } = string.Empty;
+
+    public string OwnerName { get; set; } = string.Empty;
+
+    public decimal Balance { get; set; }
+}
---
+++ server/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace server.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+
+    public int FromAccountId { get; set; }
+
+    public int ToAccountId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
