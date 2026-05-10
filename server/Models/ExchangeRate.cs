fca31a4578c3f2c363f9889edb48819e7ca768a0
Bilguun <Bilguun@DESKTOP-UL13F3Q>
2026-05-10 09:37:05 -0700

    feat: add exchange rate model

---
+++ server/Models/ExchangeRate.cs
@@ -0,0 +1,14 @@
+﻿namespace server.Models;
+
+public class ExchangeRate
+{
+    public int Id { get; set; }
+
+    public string CurrencyCode { get; set; } = string.Empty;
+
+    public decimal BuyRate { get; set; }
+
+    public decimal SellRate { get; set; }
+
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+}
