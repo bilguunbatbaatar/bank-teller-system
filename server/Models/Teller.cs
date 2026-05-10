0873f3268708a0382316f3ad92c589ab24a48b19
Bilguun <Bilguun@DESKTOP-UL13F3Q>
2026-05-10 09:37:49 -0700

    feat: add teller model

---
+++ server/Models/Teller.cs
@@ -0,0 +1,10 @@
+﻿namespace server.Models;
+
+public class Teller
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public int CounterNo { get; set; }
+}
