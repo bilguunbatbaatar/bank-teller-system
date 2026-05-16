c504ed936aec8d8c8f35db295797b9099ac50583
Munkh-Erdene050124 <munkherdeneb895@gmail.com>
2026-05-16 19:29:00 +0800

    documentation v2

--- server/Enums/TicketStatus.cs
+++ server/Enums/TicketStatus.cs
@@ -1,9 +1,16 @@
-﻿namespace server.Enums;
+namespace server.Enums;
 
+/// <summary>
+/// Очерын тасалбарын төлөвийг илэрхийлэх жагсаалт.
+/// </summary>
 public enum TicketStatus
 {
+    /// <summary> Хүлээгдэж буй. </summary>
     Waiting = 0,
+    /// <summary> Дуудагдсан. </summary>
     Called = 1,
+    /// <summary> Үйлчилгээ дууссан. </summary>
     Completed = 2,
+    /// <summary> Цуцлагдсан. </summary>
     Cancelled = 3
 }
